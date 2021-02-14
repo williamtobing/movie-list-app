76e15363ec1dd28e9f8e1c05c77cc657ef145f8a
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-14 11:43:27 +0700

    adding lazy load

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,28 +1,33 @@
+import LazyLoad from "react-lazyload";
+
 const Movie = ({ movie }) => {
   const posterLink = "https://image.tmdb.org/t/p/w1280";
 
   return (
     <div className="movie">
-      <img src={posterLink + movie.poster_path} alt="Poster" />
-      <div className="movie-info">
-        <h3>{movie.title}</h3>
-        <span
-          className={
-            movie.vote_average < 5
-              ? "red"
-              : movie.vote_average >= 8
-              ? "green"
-              : "orange"
-          }
-        >
-          {movie.vote_average}
-        </span>
-      </div>
-      <div className="overview">
-        <h3>{movie.title}</h3>
-        <h4>Release Date: {movie.release_date}</h4>
-        <p>{movie.overview}</p>
-      </div>
+      <LazyLoad throttle={200} height={300}>
+        <img src={posterLink + movie.poster_path} alt="Poster" />
+
+        <div className="movie-info">
+          <h3>{movie.title}</h3>
+          <span
+            className={
+              movie.vote_average < 5
+                ? "red"
+                : movie.vote_average >= 8
+                ? "green"
+                : "orange"
+            }
+          >
+            {movie.vote_average}
+          </span>
+        </div>
+        <div className="overview">
+          <h3>{movie.title}</h3>
+          <h4>Release Date: {movie.release_date}</h4>
+          <p>{movie.overview}</p>
+        </div>
+      </LazyLoad>
     </div>
   );
 };
